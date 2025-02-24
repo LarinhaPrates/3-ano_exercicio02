1a43c87980fc411ac09f12e2802a03260a355a34
LarinhaPrates <lara.8936@aluno.pr.senac.br>
2025-02-24 12:28:40 -0300

    24/02 12:28

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+function sortear(){
+    //pegar o total de resultados
+
+    document.querySelector('')
+
+    //pegar o menor valor
+
+    //pegar maior valor
+
+    //gerar um numero aleatório entre o menor e maior valor
+
+    //exibir o resultado do HTML
+}  
